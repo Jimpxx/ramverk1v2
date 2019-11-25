425abffaaaa11df1e3befb3a5939ccedf16873fd
Jimmy Andersson <jimmyandersson89@gmail.com>
2019-11-25 16:27:09 +0000

    Added a map to the resultpage and added some style to the map to get it to show

--- view/weather/weatherResult.php
+++ view/weather/weatherResult.php
@@ -4,6 +4,9 @@ namespace Anax\View;
 
 ?>
 
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" />
+<script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"></script>
+
 
 <h1>Weather Results</h1>
 
@@ -11,6 +14,23 @@ namespace Anax\View;
 <p>Search = <?= $search ?></p>
 <p>Time = <?= $time ?></p>
 <p>Test = <?= $test ?></p>
+
+
+<div id="map"></div>
+
+<script>
+var map = L.map('map', {
+    center: [<?= $ipInfo->latitude ?>, <?= $ipInfo->longitude ?>],
+    // center: [51.505, -0.09],
+    zoom: 13
+    
+});
+// Creating a Layer object
+var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+// Adding layer to the map
+// map.addLayer(layer);
+</script>
 
 
 <?php
